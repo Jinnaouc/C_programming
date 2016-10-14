af31688fcf55ac8f00d98f0c00cf121180c99eed
Jinna <Jinna_ouc@163.com>
2016-10-14 12:55:26 +0800

    appointment of Jingjing

---
+++ 1.3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int date;
+    cin >> date;
+    if (date == 7)
+        cout<<"YES"<<endl;
+    else if(date % 2 == 1)
+        cout <<"NO"<<endl;
+    else
+            cout<<"YES"<<endl;
+}
